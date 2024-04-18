da6d4c10173c377bd6126979e63f3c46de54d055
Artyom Vorobyov <svirex@gmail.com>
2024-04-18 17:24:34 +0400

    w

--- internal/core/services/check_accrual.go
+++ internal/core/services/check_accrual.go
@@ -7,12 +7,16 @@ import (
 )
 
 type CheckAccrualService struct {
-	dbpool *pgxpool.Pool
+	dbpool      *pgxpool.Pool
+	stopCh      chan struct{}
+	orderNumsCh chan string
 }
 
 func NewCheckAccrualService(dbpool *pgxpool.Pool) (*CheckAccrualService, error) {
 	return &CheckAccrualService{
-		dbpool: dbpool,
+		dbpool:      dbpool,
+		stopCh:      make(chan struct{}),
+		orderNumsCh: make(chan string, 10),
 	}, nil
 }
 
@@ -21,5 +25,20 @@ func (service *CheckAccrualService) Start() {
 }
 
 func (service *CheckAccrualService) Process(ctx context.Context, orderNum string) {
+	go service.generator(orderNum)
+}
 
+func (service *CheckAccrualService) generator(orderNum string) {
+	for {
+		select {
+		case <-service.stopCh:
+			return
+		default:
+			service.orderNumsCh <- orderNum
+		}
+	}
+}
+
+func (service *CheckAccrualService) Shutdown() {
+	close(service.stopCh)
 }
